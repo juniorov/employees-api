83b983be848a959b16c27b6123688b0bf0ed4d4b
Junior Obando <juniorov@gmail.com>
2022-11-02 13:11:58 -0600

    add decoded token function

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,24 @@
 const jwt = require('jsonwebtoken');
-const { EXPIRES_IN } = process.env
-const JWT_SECRET = process.env.JWT_SECRET;
+const { EXPIRES_IN, JWT_SECRET } = process.env
+// const JWT_SECRET = process.env.JWT_SECRET;
 const MINUTES = EXPIRES_IN ?? 2;
 
-module.exports = {
-    createToken: payload => {
-        const token = jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + (60 * MINUTES),
-            data: payload
-        }, JWT_SECRET);
+const createToken = payload => {
+    const token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * MINUTES),
+        data: payload
+    }, JWT_SECRET);
 
-        return token;
-    }
+    return token;
+}
+
+const decodeToken = data => {
+    const token = data.split(' ')[1];
+
+    return createToken(jwt.verify(token, JWT_SECRET));
+}
+
+module.exports = {
+    createToken,
+    decodeToken
 }
